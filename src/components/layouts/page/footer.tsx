1abac0b8d9e148df46f6aa83968af940614eabf8
vuk.damjanovic <vuk.damjanovic@3stepit.com>
2024-12-04 13:11:18 +0200

    Fix footer text and add hyperlink;

--- src/components/layouts/page/footer.tsx
+++ src/components/layouts/page/footer.tsx
@@ -3,7 +3,14 @@ import { Typography } from '@mui/material';
 export const Footer = () => {
   return (
     <Typography sx={{ color: '#426834', textAlign: 'center', fontSize: 12 }}>
-      © 2024 Multiplatform Messaging & Information Management System.
+      <a
+        href="https://govstack.gitbook.io/sandbox/access-demos/early-warning-tech-demo"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ textDecoration: 'none', color: '#426834' }}
+      >
+        Apache 2.0 licensed Early Warning Prototype of GovStack
+      </a>
     </Typography>
   );
 };
